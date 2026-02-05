d8544a0944d3f863468ee5662e0360244f152ea2
AlterniTech <72343266+DansDesigns@users.noreply.github.com>
2026-02-05 18:55:55 +0000

    Update install-alternix.sh

--- install-alternix.sh
+++ install-alternix.sh
@@ -118,7 +118,7 @@ sudo nala install -y \
     kalk vlc qt5-style-kvantum network-manager
 
 
-sudo nala install -y --no-install-recommends plasma-dialer spacebar plasma-discover
+sudo nala install -y --no-install-recommends plasma-dialer spacebar
 
 
 
