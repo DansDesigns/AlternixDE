a42a104c22f5691058c4dae9df6dec9b01752c9d
AlterniTech <72343266+DansDesigns@users.noreply.github.com>
2026-02-19 15:41:56 +0000

    Update install-alternix.sh

--- install-alternix.sh
+++ install-alternix.sh
@@ -49,10 +49,10 @@ echo "User setup complete. Username set to: $TARGET_USER"
 echo ""
 
 
-# ------------------- UNCOMMENT for non-RPi --------------
-echo "-[System] Installing XLibre.."
-chmod +x install_xlibre.sh
-sudo ./install_xlibre.sh
+# ------------------- error on install --------------
+#echo "-[System] Installing XLibre.."
+#chmod +x install_xlibre.sh
+#sudo ./install_xlibre.sh
 
 # ────────────────────────────────────────────────
 # 1. Install apps & dependencies
