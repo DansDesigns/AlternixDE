45d43e94f003d3602b545c0c3bb08b92ea99180f
DanDan <dandan7932@hotmail.com>
2026-02-23 17:32:54 +0000

    Update install-alternix.sh

--- install-alternix.sh
+++ install-alternix.sh
@@ -643,6 +643,8 @@ sudo mv system.so /usr/local/bin/
 echo " "
 echo "• Installing App Icons..."
 
+cd "$ALT_ROOT/Alternix"
+
 sudo cp icons/update.png /usr/share/icons/hicolor/64x64/apps/update.png
 sudo cp icons/upgrade.png /usr/share/icons/hicolor/64x64/apps/upgrade.png
 sudo cp icons/bauh.png /usr/share/icons/hicolor/64x64/apps/bauh.png
