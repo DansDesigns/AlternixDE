799322aa7924157a94943da5f174d5cb68ae2885
DanDan <dandan7932@hotmail.com>
2026-02-23 17:18:34 +0000

    Update install-update.sh

--- install-update.sh
+++ install-update.sh
@@ -10,7 +10,7 @@ echo " "
 echo " "
 echo " "
 # Root of the Alternix repo
-ALT_ROOT="Alternix"
+ALT_ROOT="$HOME/Alternix"
 
 #if [ ! -d "$ALT_ROOT" ]; then
 #    echo "ERROR: $ALT_ROOT not found. Please place install-update.sh inside ~/Alternix."
