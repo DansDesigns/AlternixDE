da35424d451eba12a7ed817be63f8f607e5bdb79
AlterniTech <72343266+DansDesigns@users.noreply.github.com>
2026-07-01 17:49:55 +0100

    Update install-update.sh

--- install-update.sh
+++ install-update.sh
@@ -29,7 +29,7 @@ echo "[Config] Updating Grub Entry.."
 sudo sed -i 's/Devuan GNU\/Linux/Alternix/g' /boot/grub/grub.cfg
 sudo update-grub
 
-sudo nala install xserver-xorg-input-synaptics -y
+sudo nala install xserver-xorg-input-synaptics xserver-xlibre-input-libinput -y
 
 echo "[Config] Installing updated configs..."
 cp -r "$ALT_ROOT/Alternix/configs/"* "$HOME/.config/"
