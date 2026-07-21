cc6031f6170c8f941934ef120bc9eaddd4a840f7
AlterniTech <72343266+DansDesigns@users.noreply.github.com>
2026-07-21 15:33:18 +0100

    update osm-power

--- Alternix/apps/osm-power.cpp
+++ Alternix/apps/osm-power.cpp
@@ -272,17 +272,20 @@ private slots:
 
     void doSleep() {
         QProcess::startDetached("osm-lockd", QStringList());
-        QProcess::startDetached("osm-sudo", QStringList() << "pm-suspend");
+        QProcess::startDetached("sudo -S pm-suspend", QStringList());
+#        QProcess::startDetached("osm-sudo", QStringList() << "pm-suspend");
         close();
     }
 
     void doReboot() {
-        QProcess::startDetached("osm-sudo", QStringList() << "reboot");
+#        QProcess::startDetached("osm-sudo", QStringList() << "reboot");
+        QProcess::startDetached("sudo -S reboot", QStringList());
         close();
     }
 
     void doPowerOff() {
-        QProcess::startDetached("osm-sudo", QStringList() << "poweroff");
+#        QProcess::startDetached("osm-sudo", QStringList() << "poweroff");
+        QProcess::startDetached("sudo -S poweroff", QStringList());
         close();
     }
 
